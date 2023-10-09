ba750e3f8aac0fd591e88bfa8e815dde3a6458d6
mishalalhaidar <malh0009@student.monash.edu>
2023-10-10 00:55:34 +1100

    more updates for the proportional map and trying a new shapefile

--- Homework week 10/vg_embed.js
+++ Homework week 10/vg_embed.js
@@ -1,2 +1,4 @@
 var vg = "stacked_bar_chart.vg.json";
+var vg2 = "proportion_symbol_map.vg.json";
 vegaEmbed("#stacked_bar_chart", vg).then(function(result){}).catch(console.error)
+vegaEmbed("#proportional_chart", vg2).then(function(result){}).catch(console.error)
