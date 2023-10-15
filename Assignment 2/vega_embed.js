99ba010bc5506fa6edc609ebfd04c8177033ac6a
mishalalhaidar <malh0009@student.monash.edu>
2023-10-15 17:44:41 +1100

    Added Donut chart and clear some datasets

--- Assignment 2/vega_embed.js
+++ Assignment 2/vega_embed.js
@@ -1,2 +1,4 @@
-var vg_1 = "state_budget_map.vg.json";
+var vg_1 = "donut_chart.vg.json";
+var vg_2 = "state_budget_stacked_chart.vg.json"
 vegaEmbed("#state_budget_map", vg_1).then(function(result){}).catch(console.error)
+// vegaEmbed("#state_chart", vg_2).then(function(result){}).catch(console.error)
