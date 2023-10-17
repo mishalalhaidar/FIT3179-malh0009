9f6a4a04e4f0d31681ba0ab13e56a12a975d845e
mishalalhaidar <malh0009@student.monash.edu>
2023-10-18 00:40:39 +1100

    more updates on the html page

--- Assignment 2/vega_embed.js
+++ Assignment 2/vega_embed.js
@@ -2,7 +2,7 @@ var vg_1 = "state_budget_map.vg.json";
 var vg_2 = "heat_map.vg.json";
 var vg_3 = "state_budget_stacked_chart.vg.json";
 var vg_4 = "donut_chart.vg.json";
-vegaEmbed("#geo-graph", vg_1).then(function(result){}).catch(console.error)
-vegaEmbed("#heat-graph", vg_2).then(function(result){}).catch(console.error)
-vegaEmbed("#area-line-graph", vg_3).then(function(result){}).catch(console.error)
-vegaEmbed("#donut-graph", vg_4).then(function(result){}).catch(console.error)
+vegaEmbed("#geo-graph", vg_1,{render:"svg",actions:false}).then(function(result){}).catch(console.error)
+vegaEmbed("#heat-graph", vg_2,{render:"svg",actions:false}).then(function(result){}).catch(console.error)
+vegaEmbed("#area-line-graph", vg_3,{render:"svg",actions:false}).then(function(result){}).catch(console.error)
+vegaEmbed("#donut-graph", vg_4,{render:"svg",actions:false}).then(function(result){}).catch(console.error)
